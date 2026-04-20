0132bfcccf81b28a37982b41961eddacf1e605ff
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-04-20 21:44:51 +0100

    list method - learnt how to add, delete, clear, extend, short hand for loop in python

--- learningProgress.py
+++ learningProgress.py
@@ -15,4 +15,17 @@ thislist.extend(thistuple)
 thislist.remove("mango")
 # The pop() method removes the specified index.
 thislist.pop(2)
-print(thislist)
+thislist.pop() # If you do not specify the index, the pop() method removes the last item.
+del thislist[0]
+# del thislist # this deletes the entirely list
+# thislist.clear() # The list still remains, but it has no content.
+# print(thislist)
+
+# A short hand for loop that will print all items in a list:
+# [print(x) for x in thislist]
+# The Syntax
+# newlist = [expression for item in iterable if condition == True]
+# newlist = [x for x in thislist if "a" in x]
+# newlist = [x for x in thislist]
+newlist = [x if x != "banana" else "orange" for x in thislist]
+print(newlist)
