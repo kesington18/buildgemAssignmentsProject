249d16c4a2c532adc749ca37e626833c338479ce
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-04-30 07:17:21 +0100

    hiding the random value and replacing it with user input

--- functions.py
+++ functions.py
@@ -3,10 +3,24 @@ import random
 word_list = ["apple", "banana", "cherry"]
 rnd = random.choice(word_list)
 
+placeholder = ["_"] * len(rnd)
 print(rnd)
-userInput = input("Guess a letter: ")
-for word in rnd:
-    if userInput == word:
+userInput = input("Guess a letter: ").lower()
+for i in range(0, len(rnd)):
+    if userInput == rnd[i]:
+        placeholder[i] = rnd[i]
         print("right")
     else:
         print("wrong")
+print("".join(placeholder))
+
+'''another Solution by angela yu'''
+display = ""
+for i in range(0, len(rnd)):
+    if userInput == rnd[i]:
+        display += rnd[i]
+        print("right")
+    else:
+        display += "_"
+        print("wrong")
+print(display)
