454a3194858c1a3e6fd26a559b5f8cad0dc548a5
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-05-01 10:47:19 +0100

    made some modification so that user can keep inputing letters until they correctly guessing or untill when there is is no _ remaining

--- functions.py
+++ functions.py
@@ -5,17 +5,20 @@ rnd = random.choice(word_list)
 
 placeholder = ["_"] * len(rnd)
 print(rnd)
-userInput = input("Guess a letter: ").lower()
-for i in range(0, len(rnd)):
-    if userInput == rnd[i]:
-        placeholder[i] = rnd[i]
-        print("right")
-    else:
-        print("wrong")
 print("".join(placeholder))
 
+while "_" in placeholder:
+    userInput = input("Guess a letter: ").lower()
+    for i in range(0, len(rnd)):
+        if userInput == rnd[i]:
+            placeholder[i] = rnd[i]
+            # print("right")
+        # else:
+            # print("wrong")
+    print("".join(placeholder))
+
 '''another Solution by angela yu'''
-display = ""
+'''display = ""
 for i in range(0, len(rnd)):
     if userInput == rnd[i]:
         display += rnd[i]
@@ -23,4 +26,4 @@ for i in range(0, len(rnd)):
     else:
         display += "_"
         print("wrong")
-print(display)
+print(display)'''
