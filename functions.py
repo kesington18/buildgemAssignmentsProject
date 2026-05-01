eaea4984a6bb56c3d9ed0471c4eeb9ac440a1adf
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-05-01 14:22:17 +0100

    addition of ascii hangman

--- functions.py
+++ functions.py
@@ -1,12 +1,63 @@
 import random
 
+hangman = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''','''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''','''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''','''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''','''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''','''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''',]
+
 word_list = ["apple", "banana", "cherry"]
 rnd = random.choice(word_list)
 
 placeholder = ["_"] * len(rnd)
 print(rnd)
 print("".join(placeholder))
-
+user_lives = 6
 while "_" in placeholder:
     userInput = input("Guess a letter: ").lower()
     for i in range(0, len(rnd)):
@@ -16,14 +67,27 @@ while "_" in placeholder:
         # else:
             # print("wrong")
     print("".join(placeholder))
+print("You win")
 
 '''another Solution by angela yu'''
-'''display = ""
-for i in range(0, len(rnd)):
-    if userInput == rnd[i]:
-        display += rnd[i]
-        print("right")
-    else:
-        display += "_"
-        print("wrong")
-print(display)'''
+'''game_over = False
+correct_letters = []
+
+while not game_over:
+    userInput = input("Guess a letter: ").lower()
+
+    display = ""
+
+    for letter in rnd:
+        if userInput == letter:
+            display += letter
+            correct_letters.append(userInput)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+    if "_" not in display:
+        game_over = True
+        print("you win!")'''
