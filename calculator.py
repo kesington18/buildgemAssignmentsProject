a315d10c990a50dafd54242498cdf5ab34731a3f
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-05-06 08:44:14 +0100

    Calculator completed build with angela solution

--- calculator.py
+++ calculator.py
@@ -1,18 +1,24 @@
 def add(a,b):
-    return f"{float(a)} + {float(b)} = {float(a) + float(b)}"
+    return a + b
 def subtract(a,b):
-    return f"{float(a)} - {float(b)} = {float(a) - float(b)}"
+    return a - b
 def multiply(a,b):
-    return f"{float(a)} * {float(b)} = {float(a) * float(b)}"
+    return a * b
 def divide(a,b):
-    return f"{float(a)} / {float(b)} = {float(a) / float(b)}"
+    return a / b
 
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
 
-print("Welcome to ym Calculator")
-numQue = int(input("What's the first number?: "))
+'''print("Welcome to ym Calculator")
+numQue = float(input("What's the first number?: "))
 print("+ \n-\n*\n/")
 opeQUe = input("Pick an operation from the above following options: ")
-num2Que = int(input("What's the second number?: "))
+num2Que = float(input("What's the second number?: "))
 
 result = 0
 
@@ -28,3 +34,29 @@ elif opeQUe == "*":
 elif opeQUe == "/":
     result = divide(numQue,num2Que)
     print(f"The result is {result}")
+'''
+
+#  Angela yu full solution
+print("Welcome to ym Calculator")
+
+def calculator():
+    should_continue = True
+    numQue = float(input("What's the first number?: "))
+
+    while should_continue:
+        for symbol in operations:
+            print(symbol)
+        opeQUe = input("Pick an operation from the above following options: ")
+        num2Que = float(input("What's the second number?: "))
+        answer =  operations[opeQUe](numQue, num2Que)
+        print(f"{numQue} {opeQUe} {num2Que} = {answer}")
+
+        choice = input(f"Type 'y' to continue with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            numQue = answer
+        else:
+            should_continue = False
+            print("\n" * 20)
+
+calculator()
