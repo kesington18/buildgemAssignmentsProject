586638882be276ff6d7a2ba58672aaae1ac5150e
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-06-04 19:24:47 +0100

    second time working with turtle in python

---
+++ oopInPython.py
@@ -0,0 +1,15 @@
+# import another_variable
+
+# print(another_variable.another_variable)
+
+from turtle import Turtle, Screen
+
+timi = Turtle()
+print(timi)
+timi.shape("turtle")
+timi.color("red")
+timi.forward(100)
+
+myScreen = Screen()
+print(myScreen.canvheight)
+myScreen.exitonclick()
