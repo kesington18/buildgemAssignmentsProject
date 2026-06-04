b2c18823b531a36e0799c1cb3df83a51375384ae
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-06-04 23:27:02 +0100

    learn about import other packah=ges like prettytable for creating table like structure of things

--- oopInPython.py
+++ oopInPython.py
@@ -2,14 +2,33 @@
 
 # print(another_variable.another_variable)
 
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timi = Turtle()
+# print(timi)
+# timi.shape("turtle")
+# timi.color("red")
+# timi.forward(100)
+#
+# myScreen = Screen()
+# print(myScreen.canvheight)
+# myScreen.exitonclick()
 
-timi = Turtle()
-print(timi)
-timi.shape("turtle")
-timi.color("red")
-timi.forward(100)
+# i had to install prettytables in my interpreter
+from prettytable import PrettyTable as pt
 
-myScreen = Screen()
-print(myScreen.canvheight)
-myScreen.exitonclick()
+table = pt()
+table.add_column("Pokemon name", [
+    "Pikachu",
+    "Squirtle",
+    "Charmander",
+])
+table.add_column("Type", [
+    "Electric",
+    "Water",
+    "Fire",
+])
+
+table.align = "l"
+
+print(table)
