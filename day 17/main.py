45955c78e08ce8ab1e942e5c96e0f3071f6e062f
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-07-16 22:23:52 +0100

    learnt more about __init__ in oop as it is the constructor that is like the attribute of the class

--- day 17/main.py
+++ day 17/main.py
@@ -1,7 +1,16 @@
 class User:
+
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        print("user created")
     """Models the user."""
     pass
 
-user_1 = User()
-user_1.id = "001"
-user_1.name = "angela"
+user_1 = User("001", "eniola")
+print(user_1.id)
+print(user_1.followers)
+
+
+# print(user_1.name)
